4c82af238a356da20c7b58874a6b92fdc201479c
Soyoo.kim <soyoo0819@naver.com>
2023-04-12 17:57:43 +0900

    first commit

---
+++ src/main/java/com/ssoyoo/projectboard/SsoyooProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ssoyoo.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoyooProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoyooProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssoyoo/projectboard/SsoyooProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssoyoo.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoyooProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
